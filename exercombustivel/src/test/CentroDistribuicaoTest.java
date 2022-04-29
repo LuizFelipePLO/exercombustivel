bfe1713970d9317c6a45816748be728f24a4177f
LuizFelipePorto <83476859+LuizFelipePorto@users.noreply.github.com>
2022-04-29 15:14:18 -0300

    Testes metodos get + recebe

--- exercombustivel/src/test/CentroDistribuicaoTest.java
+++ exercombustivel/src/test/CentroDistribuicaoTest.java
@@ -1,8 +1,3 @@
-import java.beans.Transient;
-
-import com.lfoliveira.CentroDistribuicao;
-import com.lfoliveira.CentroDistribuicao.SITUACAO;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -49,40 +44,107 @@ public class CentroDistribuicaoTest {
     @Test
     public void getSituacaoTest() {
 
+        setUpTanqueNormal();
+        SITUACAO resultNormal = cd.getSituacao();
+        Assertions.assertEquals(SITUACAO.NORMAL, resultNormal);
+
+        setUpTanqueSobraviso();
+        SITUACAO resultSobraviso = cd.getSituacao();
+        Assertions.assertEquals(SITUACAO.SOBRAVISO, resultSobraviso);
+
+        setUpTanqueEmergencia();
+        SITUACAO resultEmergencia = cd.getSituacao();
+        Assertions.assertEquals(SITUACAO.EMERGENCIA, resultEmergencia);
+
     }
 
     @Test
     public void gettGasolinaTest() {
+
+        setUpTanqueNormal();
+        int result = cd.gettGasolina();
+        Assertions.assertEquals(10000, result);
 
     }
 
     @Test
     public void gettAditivoTest() {
 
+        setUpTanqueNormal();
+        int result = cd.gettAditivo();
+        Assertions.assertEquals(500, result);
+
     }
 
     @Test
     public void gettAlcool1Test() {
+
+        setUpTanqueNormal();
+        int result = cd.gettAlcool1();
+        Assertions.assertEquals(1250, result);
 
     }
 
     @Test
     public void gettAlcool2Test() {
 
+        setUpTanqueNormal();
+        int result = cd.gettAlcool2();
+        Assertions.assertEquals(1250, result);
+
     }
 
     @Test
     public void recebeAditivoTest() {
+
+        setUpTanqueSobraviso();
+        int result = cd.recebeAditivo(100);
+        Assertion.assertEquals(400, result);
+
+    }
+
+    @Test
+    public void recebeAditivoValorInvalidoTest() {
+
+        setUpTanqueNormal();
+        int result = cd.recebeAditivo(150);
+        Assertions.assertEquals(-1, result);
 
     }
 
     @Test
     public void recebeGasolinaTest() {
 
+        setUpTanqueSobraviso();
+        int result = cd.recebeGasolina(1000);
+        Assertions.assertEquals(5000, result);
+
+    }
+
+    @Test
+    public void recebeGasolinaValorInvalidoTest() {
+
+        setUpTanqueNormal();
+        int result = cd.recebeGasolina(1500);
+        Assertions.assertEquals(-1, result);
+
     }
 
     @Test
     public void recebeAlcoolTest() {
+
+        setUpTanqueSobraviso();
+        int result = cd.recebeGasolina(200);
+        Assertions.assertEquals(1450, result);
+
+    }
+
+    @Test
+    public void recebeAlcoolValorInvalidoTest() {
+
+        setUpTanqueNormal();
+        int result = cd.recebeAlcool(150);
+        Assertions.assertEquals(-1, result);
 
     }
 
